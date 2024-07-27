0a3dab25560d09a383ed2a44ea2af4637a2a4316
Clemens Kruse <clemens.kruse@externer.at>
2024-07-27 14:01:31 +0200

    update colors for highlighting

--- init.lua
+++ init.lua
@@ -250,44 +250,47 @@ function clemens_highlight()
   -- Get the current buffer's filename
   local filename = vim.fn.expand('%:t') -- Get the filename without the path
 
-  -- Helper function to run git status and get a list of filenames
-  local function get_git_filenames(grep_pattern)
+  -- Capture the list of files from git status and extract filenames
+  local function get_files_from_git_status(grep_pattern)
     local handle = io.popen(
       'git status --porcelain=v2 | sed -E \'s/[0-9]{6} [0-9]{6} [0-9]{6} [a-f0-9]{40} [a-f0-9]{40} //\' | grep "' ..
       grep_pattern .. '" | awk -F\'[[:space:]]\' \'{print $(NF)}\' | awk -F\'/\' \'{print $NF}\''
     )
     local result = handle:read("*a")
     handle:close()
-
-    -- Split the output into lines
-    local file_list = {}
-    for file in result:gmatch("[^\r\n]+") do
-      table.insert(file_list, file)
-    end
-    return file_list
+    return result
   end
 
-  -- Get the list of filenames to highlight in green and red
-  local files_to_highlight_green = get_git_filenames("M. ")
-  local files_to_highlight_red = get_git_filenames(".M ")
+  -- Get the list of files to highlight in green and red
+  local files_to_highlight_green = get_files_from_git_status("M. ")
+  local files_to_highlight_red = get_files_from_git_status(".M ")
+
+  -- Split the output into lines
+  local function split_lines(input)
+    local lines = {}
+    for line in input:gmatch("[^\r\n]+") do
+      table.insert(lines, line)
+    end
+    return lines
+  end
+
+  local file_list_green = split_lines(files_to_highlight_green)
+  local file_list_red = split_lines(files_to_highlight_red)
 
   -- Start with the tree command
   local cmd = 'tree -I "bin|obj" -F'
 
   -- Add existing highlighting for the current filename
-  cmd = cmd .. ' | sed "s/' .. filename .. '/\\x1b[1m& \\x1b[0m/g"'
-  cmd = cmd .. ' | sed "s/' .. filename .. '/\\x1b[1m& <<\\x1b[0m/g"'
+  cmd = cmd .. ' | sed "s/' .. filename .. '/\\x1b[38;2;255;255;255m\\x1b[48;2;213;196;161m&\\x1b[0m/g"'
 
-  -- Helper function to add highlighting for a list of files
-  local function add_highlighting(file_list, color_code)
-    for _, file in ipairs(file_list) do
-      cmd = cmd .. ' | sed "s/' .. file .. '/\\x1b[' .. color_code .. 'm&\\x1b[0m/g"'
-    end
+  -- Add highlighting for additional files
+  for _, file in ipairs(file_list_green) do
+    cmd = cmd .. ' | sed "s/' .. file .. '/\\x1b[38;2;0;128;0m&\\x1b[0m/g"'
   end
 
-  -- Add highlighting for green and red files
-  add_highlighting(files_to_highlight_green, "32") -- green
-  add_highlighting(files_to_highlight_red, "31")   -- red
+  for _, file in ipairs(file_list_red) do
+    cmd = cmd .. ' | sed "s/' .. file .. '/\\x1b[31m&\\x1b[0m/g"'
+  end
 
   -- Open a new vertical split terminal and execute the command
   vim.api.nvim_command('vnew')                               -- Open a vertical split
@@ -310,3 +313,21 @@ end
 
 -- Create a command to call the function
 vim.api.nvim_command('command! ShowFileInTree lua clemens_highlight()')
+
+
+
+-- Function to get the RGB color of a highlight group
+local function get_font_color(highlight_group)
+  local hl = vim.api.nvim_get_hl_by_name(highlight_group, true)
+  local fg = hl.fg or hl.foreground
+  if fg then
+    -- Convert from hex to RGB
+    return string.format("#%06x", fg)
+  else
+    return nil
+  end
+end
+
+-- Example usage to get the font color for 'Normal'
+local font_color = get_font_color('Normal')
+print("Font color for 'Normal': " .. font_color)
